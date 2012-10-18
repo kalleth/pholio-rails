6c787a25a9417febfffe67c974e629e7decb5c51
Tom Russell <tom.russell@initforthe.com>
2012-10-18 20:44:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PholioRails::Application.config.secret_token = 'c77b4d33fc72a941bc1353baabae832d008d116a458d6143085ce6b5fab4e90ac29a88be1149784b2a3f9d889af0600df1046ed4600af9cea77c8c807ea7e7fe'
