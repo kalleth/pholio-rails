d6be204ca5f9275dca057b64be402c25b129de33
Tom Russell <tom.russell@initforthe.com>
2012-10-19 02:01:30 +0100

    previous and next links. Now im really going to bed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,14 @@ class Photo < ActiveRecord::Base
   serialize :meta
   before_save :set_exif
 
+  def previous  
+    self.class.first(:conditions => ['id < ?', self.id], :limit => 1, :order => "id DESC")
+  end
+
+  def next
+    self.class.first(:conditions => ['id > ?', self.id], :limit => 1, :order => "id ASC")
+  end
+
   def set_exif
     self.meta = image.get_exif.delete_if { |k,v| v.nil? || v.empty? }
   end
